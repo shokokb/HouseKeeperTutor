4992efc5c2ccdeca762802ffd0eafdee659297de
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2022-02-07 22:05:23 +0900

    モデルを追加

---
+++ Model/HouseKeeping.swift
@@ -0,0 +1,17 @@
+//
+//  HouseKeepingModel.swift
+//  HouseKeeperTutor
+//
+//  Created by Shoko KOBAYASHI on 2022/02/07.
+//
+
+import Foundation
+//import SwiftUI
+//import CoreLocation
+
+struct HouseKeeping: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var category: String
+    var step: [String]
+}
