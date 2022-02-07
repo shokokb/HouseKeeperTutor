685efebab0acc97b35a4ffc1a97ee0663e8641ee
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2022-02-07 21:52:03 +0900

    行を追加

---
+++ HouseKeeperTutor/Views/HouseKeeperingRow.swift
@@ -0,0 +1,20 @@
+//
+//  HouseKeeperingRow.swift
+//  HouseKeeperTutor
+//
+//  Created by Shoko KOBAYASHI on 2022/02/07.
+//
+
+import SwiftUI
+
+struct HouseKeeperingRow: View {
+    var body: some View {
+        Text("Hello, HouseKeeping")
+    }
+}
+
+struct HouseKeeperingRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HouseKeeperingRow()
+    }
+}
